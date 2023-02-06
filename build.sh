fedd61af606ca5fac77b581c9b1500d57a811496
Ajanth Kathirkamu <ajanth.kathirkamu@clark.de>
2023-02-06 05:21:05 +0100

    CI: update build script

--- build.sh
+++ build.sh
@@ -1,30 +1,34 @@
-echo "1/3 Building frontend..."
+echo "1/4 Installing and Building frontend..."
 cd client
+npm install
+npm rebuild node-sass
 npm run build
 rm build/index.html
-echo "2/3 Creating api folder..."
+echo "2/4 Installing composer dependencies"
 cd ../server
+composer install
+echo "3/4 Creating api folder..."
 echo -n > config.php
 mkdir files2
-mv files/.htaccess files2
-rm -rf files/
-mv files2 files
+cp files/.htaccess files2
 cd ..
 mkdir api
-mv server/index.php api
-mv server/.htaccess api
-mv server/composer.json api
-mv server/composer.lock api
-mv server/controllers api
-mv server/data api
-mv server/libs api
-mv server/models api
-mv server/vendor api
-mv server/files api
+mkdir api/files
+cp server/index.php api
+cp server/.htaccess api
+cp server/composer.json api
+cp server/composer.lock api
+cp -a server/controllers api
+cp -a server/data api
+cp -a server/libs api
+cp -a server/models api
+cp -a server/vendor api
+cp -a server/files2/. api/files
+rm -rf server/files2
 cp server/config.php api
 chmod -R 755 .
 cp client/src/index.php client/build
-echo "3/3 Generating zip..."
+echo "4/4 Generating zip..."
 cd client/build
 zip opensupports_dev.zip index.php
 zip -u opensupports_dev.zip .htaccess
